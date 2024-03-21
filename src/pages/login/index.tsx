fe492a7a1827b6ca49f087780aed1f57672702da
Alan de Souza <alaan.correia.08@gmail.com>
2024-03-21 00:54:31 -0300

    Login page and responsiveness for different screens

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,14 +10,14 @@ export const Login = () => {
   }
   return (
     <main className="w-full h-screen flex justify-center items-center bg-[#327D6B]">
-      <section className="w-[80%] h-[80%] flex justify-between items-center border-1 border-zinc-400">
-        <div className="ps-8[50px] w-1/2 bg-gradient-to-r from-emerald-800 to-black flex justify-center h-full items-center ">
-        <div className="h-[30rem] w-[30rem] flex justify-center align-center items-center">
+      <section className="w-[80%] h-[80%] flex justify-between items-center border-1 border-zinc-400 h-[80%] lg:w-[2100px] lg:h-[90%] lg:flex lg:flex-col lg:flex-row lg:justify-between lg:items-center lg:border-1 lg:border-zinc-400">
+        <div className="ps-8[50px] w-1/2 bg-gradient-to-r from-emerald-800 to-black flex justify-center h-full items-center lg:h-[30%] lg:w-[65%]">
+        <div className="h-[30rem] w-[30rem] lg:h-[20rem] lg:w-[20rem] flex justify-center align-center items-center ">
             <img src={logo} alt="Logo ConnectFeso"/>
           </div>
         </div>
-        <div className="w-1/2 px-20 gap-2 flex-col flex justify-center h-full items-center bg-white">
-          <h1 className="w-full text-5xl text-start text-2xl font-bold mb-2 border-b-1 border-zinc-400 pb-2 text-emerald-600 font-bold">
+        <div className="w-1/2 px-20 gap-2 flex-col flex justify-center h-full bg-white lg:h-[100%] lg:w-[65%] sm:px-10">
+          <h1 className="w-full text-5xl text-2xl font-bold mb-2 border-b-1 border-zinc-400 pb-2 text-emerald-600 font-bold justify-center">
             Login
           </h1>
           <p className="w-full text-start text-sm mb-2 text-base">
@@ -47,7 +47,7 @@ export const Login = () => {
             </p>
             <button
   type="submit"
-  className="w-[20rem] h-[3rem] bg-emerald-600 text-white text-lg font-semibold rounded-full bg-gradient-to-r from-slate-700 via-emerald-600 to-emerald-600 hover:via-teal-300"
+  className="w-[20rem] h-[3rem] bg-emerald-600 text-white text-lg font-semibold rounded-full bg-gradient-to-r from-slate-700 via-emerald-600 to-emerald-600 hover:via-teal-300 md:w-[15rem] 2sm:w-[10rem]"
 >
               Entrar
             </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+
+      'xl': {'max': '1280px'},
+
+      'lg': {'max': '1175px'},
+
+      'md': {'max': '738px'},
+
+      'sm': {'max': '640px'},
+
+      '2sm': {'max': '493px'}
+    }
   },
   plugins: [],
 }
-
