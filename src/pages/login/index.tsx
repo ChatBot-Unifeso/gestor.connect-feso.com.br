74d0a88f6582fce9ee27fff28cd3ed021e331986
Alan de Souza <alaan.correia.08@gmail.com>
2024-03-22 23:54:42 -0300

    Login screen finished

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,21 +9,22 @@ export const Login = () => {
     console.log(email, password)
   }
   return (
-    <main className="w-full h-screen flex justify-center items-center bg-[#327D6B]">
-      <section className="w-[80%] h-[80%] flex justify-between items-center border-1 border-zinc-400 h-[80%] lg:w-[2100px] lg:h-[90%] lg:flex lg:flex-col lg:flex-row lg:justify-between lg:items-center lg:border-1 lg:border-zinc-400">
-        <div className="ps-8[50px] w-1/2 bg-gradient-to-r from-emerald-800 to-black flex justify-center h-full items-center lg:h-[30%] lg:w-[65%]">
-        <div className="h-[30rem] w-[30rem] lg:h-[20rem] lg:w-[20rem] flex justify-center align-center items-center ">
+    <main className="w-full h-screen flex justify-center items-center bg-[#327D6B] lg:w-[100%]">
+      <section className="shadow-[0px_0px_30px_1px_#000] w-[65%] h-[80%] 
+      flex justify-between items-center border-1 border-zinc-400 lg:h-[90%] lg:flex lg:flex-col lg:flex-row lg:justify-between lg:items-center lg:border-1 lg:border-zinc-400 2sm:w-[90%] 2sm:h-[95%]">
+        <div className="w-full ps-8[50px] w-1/2 bg-gradient-to-r from-emerald-800 to-black flex justify-center h-full items-center lg:h-[30%]">
+        <div className="w-full h-full lg:h-[20rem] lg:w-[20rem] flex justify-center align-center items-center ">
             <img src={logo} alt="Logo ConnectFeso"/>
           </div>
         </div>
-        <div className="w-1/2 px-20 gap-2 flex-col flex justify-center h-full bg-white lg:h-[100%] lg:w-[65%] sm:px-10">
+        <div className="w-full h-full w-1/2 px-20 gap-2 flex-col flex justify-center  bg-white sm:px-10">
           <h1 className="w-full text-5xl text-2xl font-bold mb-2 border-b-1 border-zinc-400 pb-2 text-emerald-600 font-bold justify-center">
             Login
           </h1>
           <p className="w-full text-start text-sm mb-2 text-base">
             Bem-vindo de volta! Insira seu login para entrar em sua conta.
           </p>
-          <form onSubmit={submit} className="flex flex-col w-full  gap-8 items-center pt-10">
+          <form onSubmit={submit} className="flex flex-col w-full  gap-8 items-center pt-10 lg:gap-3 2sm:pt-2">
             <div className="flex w-full flex-col mb-2 gap-2">
               <label htmlFor="">E-mail</label>
               <input
@@ -47,8 +48,7 @@ export const Login = () => {
             </p>
             <button
   type="submit"
-  className="w-[20rem] h-[3rem] bg-emerald-600 text-white text-lg font-semibold rounded-full bg-gradient-to-r from-slate-700 via-emerald-600 to-emerald-600 hover:via-teal-300 md:w-[15rem] 2sm:w-[10rem]"
->
+  className="w-[20rem] h-[3rem] bg-emerald-600 text-white text-lg font-semibold rounded-full bg-gradient-to-r from-slate-700 via-emerald-600 to-emerald-600 hover:via-teal-300 md:w-[15rem] 2sm:w-[10rem]">
               Entrar
             </button>
           </form>
