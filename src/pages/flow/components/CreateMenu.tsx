38ef2998be50d059f4df7e5bdde05b7452dbe883
Gustaxz <guxtavogusteible@gmail.com>
2024-04-03 12:23:22 -0300

    feat: adicionando menu simples com placeholders

---
+++ src/pages/flow/components/CreateMenu.tsx
@@ -0,0 +1,63 @@
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogCloseButton,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogOverlay,
+	useDisclosure,
+} from "@chakra-ui/react"
+import { Plus } from "phosphor-react"
+import { useRef } from "react"
+import { MenuNode } from ".."
+
+interface CreateMenuDialogProps {
+	addMenuNode(menuNode: MenuNode): void
+}
+
+export function CreateMenuDialog({ addMenuNode }: CreateMenuDialogProps) {
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	const cancelRef = useRef()
+
+	function createMenu() {
+		const randNum = Math.floor(Math.random() * 100)
+		addMenuNode({
+			id: `${randNum}`,
+			title: `Menu ${randNum}`,
+		})
+		onClose()
+	}
+
+	return (
+		<>
+			<button
+				onClick={onOpen}
+				className="p-3 bg-green-600 rounded-md absolute top-4 right-4 text-white hover:bg-opacity-90 z-10"
+			>
+				<Plus size={24} weight="bold" />
+			</button>
+			<AlertDialog
+				motionPreset="slideInBottom"
+				leastDestructiveRef={cancelRef as any}
+				onClose={onClose}
+				isOpen={isOpen}
+				isCentered
+			>
+				<AlertDialogOverlay />
+
+				<AlertDialogContent>
+					<AlertDialogHeader>Adicionar menu</AlertDialogHeader>
+					<AlertDialogCloseButton />
+					<AlertDialogBody>
+						<button
+							className="bg-green-600 text-white font-bold px-3 py-2 rounded-md"
+							onClick={createMenu}
+						>
+							Adicionar menu
+						</button>
+					</AlertDialogBody>
+				</AlertDialogContent>
+			</AlertDialog>
+		</>
+	)
+}
--- src/pages/flow/index.tsx
+++ src/pages/flow/index.tsx
@@ -7,9 +7,16 @@ import {
 	Node as NodeRF,
 	applyEdgeChanges,
 	applyNodeChanges,
+	useReactFlow,
 } from "reactflow"
 import { ManageFlow } from "../manage-flow"
 import { Sidebar } from "../sidebar"
+import { CreateMenuDialog } from "./components/CreateMenu"
+
+export interface MenuNode {
+	id: string
+	title: string
+}
 
 export interface MenuProps {
 	id: string
@@ -24,8 +31,8 @@ const menusStd: MenuProps[] = [
 		id: "id-1",
 		title: "Menu 1",
 		nodes: [
-			{ id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-			{ id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+			{ id: "1", position: { x: 0, y: 0 }, data: { label: "1" }, height: 15, width: 15 },
+			{ id: "2", position: { x: 0, y: 100 }, data: { label: "2" }, height: 15, width: 15 },
 		],
 		edges: [{ id: "e1-2", source: "1", target: "2" }],
 	},
@@ -33,8 +40,20 @@ const menusStd: MenuProps[] = [
 		id: "id-2",
 		title: "Menu 2",
 		nodes: [
-			{ id: "3", position: { x: 5, y: 10 }, data: { label: "teste 1" } },
-			{ id: "4", position: { x: 0, y: 100 }, data: { label: "teste 2" } },
+			{
+				id: "3",
+				position: { x: 5, y: 10 },
+				data: { label: "teste 1" },
+				height: 15,
+				width: 15,
+			},
+			{
+				id: "4",
+				position: { x: 0, y: 100 },
+				data: { label: "teste 2" },
+				height: 15,
+				width: 15,
+			},
 		],
 		edges: [{ id: "e3-4", source: "4", target: "3" }],
 	},
@@ -91,6 +110,32 @@ export const Flow = () => {
 		}
 	}
 
+	const { zoomIn, zoomOut, setCenter } = useReactFlow()
+
+	function addMenuNode(menuNode: MenuNode) {
+		if (!menu) return
+		const lenMenu = menu.nodes.length - 1
+		const someNode = menu.nodes[lenMenu]
+		const position = {
+			x: someNode.position.x + 256,
+			y: someNode.position.y,
+		}
+		const newNodes = [
+			...menu.nodes,
+			{ id: menuNode.id, position, data: { label: menuNode.title }, height: 15, width: 15 },
+		]
+		setNodes(newNodes)
+		setMenu({ ...menu, nodes: newNodes })
+		setMenus((ms) => {
+			const newMenus = ms.map((m) => {
+				if (m.id === menu.id) return { ...m, nodes: newNodes }
+				return m
+			})
+			return newMenus
+		})
+		setCenter(position.x, position.y, { zoom: 2.3, duration: 1000 })
+	}
+
 	return (
 		<div className="flex w-screen">
 			<Sidebar
@@ -99,14 +144,17 @@ export const Flow = () => {
 				handleMenuChange={handleMenuChange}
 				deleteMenu={deleteMenu}
 			/>
+			<CreateMenuDialog addMenuNode={addMenuNode} />
 			<section className="h-screen w-full flex justify-between items-center border-1 border-zinc-400">
 				{menu ? (
-					<ManageFlow
-						nodes={nodes}
-						edges={edges}
-						onEdgesChange={onEdgesChange}
-						onNodesChange={onNodesChange}
-					/>
+					<>
+						<ManageFlow
+							nodes={nodes}
+							edges={edges}
+							onEdgesChange={onEdgesChange}
+							onNodesChange={onNodesChange}
+						/>
+					</>
 				) : (
 					<div className="flex justify-center items-center w-full h-full">
 						<h1 className="text-4xl font-bold text-zinc-400">Sem menus</h1>
